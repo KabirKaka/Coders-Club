a6ce414b3bcf7ba876c729eb68eb1618c3da5e02
Ataib Saboor <ataibsaboork@gmail.com>
2023-04-14 16:15:37 +0500

    Created base.html template in \templates directory in the root directory of project and updated leaderboard.html to extend from the base template

--- coding_club_project/settings.py
+++ coding_club_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'coding_club_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
